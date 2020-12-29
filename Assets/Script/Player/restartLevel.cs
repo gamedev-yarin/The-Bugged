3505cd1df751d481e5a14081eceb5f6c5018b52e
yarin Shapira <yariniii19@gmail.com>
2020-12-29 15:10:07 +0200

    Fix a problem that the box falls off the map and adding an option to restart the level

---
+++ Assets/Script/Player/restartLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*
+ * This script gives the player the option to restart the level.
+ */
+
+public class restartLevel : MonoBehaviour
+{
+    [SerializeField] string sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ Assets/Script/resetBox.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script initializes the position of the object when it falls below the y-axis at a given number.
+ */
+
+public class resetBox : MonoBehaviour
+{
+
+    [SerializeField] float boundary = -1f;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < boundary) transform.position = startPos;
+    }
+}
